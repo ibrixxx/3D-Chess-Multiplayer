9cde41d535bae515a95e6d1d4dbbcd1a275d3607
Ibrahim <53277964+ibrixxx@users.noreply.github.com>
2021-05-01 14:37:27 +0200

    Add files via upload

---
+++ Utils/MaterialSetter.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MaterialSetter : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    public MeshRenderer meshRenderer {
+        get{
+            if(_meshRenderer == null)
+                _meshRenderer = GetComponent<MeshRenderer>();
+            return _meshRenderer;
+        }
+    }
+
+    public void SetSingleMaterial(Material material) {
+        meshRenderer.material = material;
+    }
+}
